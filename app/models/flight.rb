c73630f8aa8219edc4f671798d92ab99c2a757de
github username <github email address>
2019-10-03 15:02:09 -0400

    DONE

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,19 +2,4 @@
 class Flight < ActiveRecord::Base
     has_many :tickets
     has_many :travelers, through: :tickets
-
-    # def self.create
-    #     flight = Flight.new(airline: airline,
-    #                         model: model, 
-    #                         origin: origin,
-    #                         destination: destination,
-    #                         departure: departure,
-    #                         arrival: arrival)
-    #     flight.save
-    #     flight
-    # end
-
-
-
-
 end
